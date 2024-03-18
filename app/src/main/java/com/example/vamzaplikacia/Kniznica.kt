2b4ef046618724a804bf6e740c452b41665c9b3a
huzevkova <huzevkova@stud.uniza.sk>
2024-03-19 00:54:57 +0100

    pridanie prvych tried - Kniha (uklada info o konkretnej knihe) a ZoznamKnih (struktura na ukladanie a pracu s knihami)

---
+++ app/src/main/java/com/example/vamzaplikacia/Kniznica.kt
@@ -0,0 +1,93 @@
+package com.example.vamzaplikacia
+
+import java.util.Date
+
+class Kniha(val nazov: String, val autor: String, val rokVydania: Int, val datumPridania: Date = Date()) {
+    var favorit: Boolean = false
+    var precitana: Boolean = false
+    var naNeskor: Boolean = false
+    var popis: String = "Bez popisu."
+
+    override fun toString(): String {
+        return "$nazov, $autor, $rokVydania"
+    }
+}
+
+class ZoznamKnih() {
+    private val knihy: MutableList<Kniha> = mutableListOf()
+    private lateinit var nazovZoznamu: String
+
+    constructor(nazovZoznamu: String): this() {
+        this.nazovZoznamu = nazovZoznamu
+    }
+
+    fun pridajKnihu(novaKniha: Kniha) {
+        if (!knihy.contains(novaKniha)) {
+            knihy.add(novaKniha)
+        }
+    }
+
+    fun odoberKnihu(index: Int) {
+        if (index in 0 until knihy.size) {
+            knihy.removeAt(index)
+        }
+    }
+
+    fun get(index: Int): Kniha? {
+        if (index in 0 until knihy.size) {
+            return knihy[index]
+        }
+        return null
+    }
+
+    fun najdiKnihuPodla(text: String): Kniha? {
+        return knihy.find {
+            (it.nazov == text || it.autor == text || it.rokVydania.toString() == text)
+        }
+    }
+
+    /**
+     * Vypise zoznam v poradi v akom boli knihy pridavane - originalny zoznam.
+     */
+    fun vypisZoznam() {
+        if (knihy.isEmpty()) {
+            println("Zoznam je prázdny.")
+        } else {
+            println("Knihy v zozname${if (!this::nazovZoznamu.isInitialized) "" else " $nazovZoznamu"}:")
+            this.zoradPodla { kniha1 -> kniha1.datumPridania.toString()}
+            knihy.forEachIndexed { index, kniha -> println("${index+1}. ${kniha.toString()}") }
+        }
+    }
+
+    /**
+     * Vypise zoznam zoradení podľa posledného zoradenia.
+     */
+    fun vypisZoradene() {
+        println("Zoradene knihy:")
+        knihy.forEachIndexed { index, kniha -> println("${index+1}. ${kniha.toString()}") }
+    }
+
+    fun vratPodlaPodmienky(podmienka: (Kniha) -> Boolean): List<Kniha> {
+        val skupina = knihy.groupBy(podmienka)
+        return skupina[true] ?: mutableListOf<Kniha>()
+    }
+
+    fun zoradPodla(podmienka: (Kniha) -> String) {
+        knihy.sortBy(podmienka)
+    }
+    inner class KnihyIterator : Iterator<Kniha> {
+        private var index = 0
+
+        override fun hasNext(): Boolean {
+            return index < knihy.size
+        }
+
+        override fun next(): Kniha {
+            return knihy[index++]
+        }
+    }
+
+    fun iterator(): KnihyIterator {
+        return KnihyIterator()
+    }
+}
--- app/src/main/java/com/example/vamzaplikacia/MainActivity.kt
+++ app/src/main/java/com/example/vamzaplikacia/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.vamzaplikacia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +14,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.vamzaplikacia.ui.theme.VAMZAplikaciaTheme
+import java.util.Date
+
+val zoznamKnih = ZoznamKnih()
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +28,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    VytvorZoznam()
+                    VypisanieKnih(zoznam = zoznamKnih)
                 }
             }
         }
     }
+}
+
+fun VytvorZoznam() {
+    zoznamKnih.pridajKnihu(Kniha("Veľký Gatsby", "F. Scott Fitzgerald", 1925))
+    zoznamKnih.pridajKnihu(Kniha("Alchymista", "Paulo Coelho", 1988, Date(2000, 2, 2)))
+    zoznamKnih.pridajKnihu(Kniha("1984", "George Orwell", 1949, Date(2003, 9, 7)))
+    zoznamKnih.pridajKnihu(Kniha("Hobit", "J. R. R. Tolkien", 1937))
 }
 
 @Composable
@@ -37,10 +51,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun VypisanieKnih(zoznam: ZoznamKnih) {
+    Row (horizontalArrangement = Arrangement.SpaceBetween) {
+        Column {
+            for (kniha in zoznam.iterator()) {
+                Text(kniha.toString())
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     VAMZAplikaciaTheme {
-        Greeting("Android")
+        VytvorZoznam()
+        VypisanieKnih(zoznam = zoznamKnih)
     }
 }
