13b5cb110ebb9d87ea3d1be878698045b94c2683
huzevkova <huzevkova@stud.uniza.sk>
2024-03-25 23:50:07 +0100

    uprava typografie, prve zaclenenenie Material Design - list knih

--- app/src/main/java/com/example/vamzaplikacia/MainActivity.kt
+++ app/src/main/java/com/example/vamzaplikacia/MainActivity.kt
@@ -3,16 +3,29 @@ package com.example.vamzaplikacia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.vamzaplikacia.knihy.Kniha
 import com.example.vamzaplikacia.knihy.Vstup
 import com.example.vamzaplikacia.knihy.ZoznamKnih
@@ -20,31 +33,44 @@ import com.example.vamzaplikacia.ui.theme.VAMZAplikaciaTheme
 import java.util.Date
 
 val zoznamKnih = ZoznamKnih()
-val vyroky = Vstup().NacitajVyroky()
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             VAMZAplikaciaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    VytvorZoznam()
-                    VypisanieKnih(zoznam = zoznamKnih)
-                }
+                VytvorZoznam()
+                VypisanieKnih(zoznam = zoznamKnih)
             }
         }
     }
 }
 
 fun VytvorZoznam() {
-    zoznamKnih.pridajKnihu(Kniha("Veľký Gatsby", "F. Scott Fitzgerald", 1925))
-    zoznamKnih.pridajKnihu(Kniha("Alchymista", "Paulo Coelho", 1988, Date(2000, 2, 2)))
-    zoznamKnih.pridajKnihu(Kniha("1984", "George Orwell", 1949, Date(2003, 9, 7)))
-    zoznamKnih.pridajKnihu(Kniha("Hobit", "J. R. R. Tolkien", 1937))
+    zoznamKnih.pridajKnihu(Kniha(
+        "Veľký Gatsby",
+        "F. Scott Fitzgerald",
+        1925,
+    ))
+    zoznamKnih.pridajKnihu(Kniha(
+        "Alchymista",
+        "Paulo Coelho",
+        1988,
+        Date(2000, 2, 2),
+    ))
+    zoznamKnih.get(1)?.favorit = true;
+    zoznamKnih.pridajKnihu(Kniha(
+        "1984",
+        "George Orwell",
+        1949,
+        Date(2003, 9, 7)
+    ))
+    zoznamKnih.pridajKnihu(Kniha(
+        "Hobit",
+        "J. R. R. Tolkien",
+        1937,
+        obrazok = R.drawable.hobit
+    ))
 }
 
 @Composable
@@ -60,14 +86,47 @@ fun VypisanieKnih(zoznam: ZoznamKnih) {
     Row (horizontalArrangement = Arrangement.SpaceBetween) {
         Column {
             for (kniha in zoznam.iterator()) {
-                Text(kniha.toString())
+                ListItem(
+                    headlineContent = { Text(kniha.nazov) },
+                    supportingContent = { Text(kniha.autor + ", " + kniha.rokVydania) },
+                    trailingContent = {
+                        if (kniha.favorit) {
+                            Icon(
+                                Icons.Filled.Favorite,
+                                contentDescription = "Localized description",
+                            )
+                        }
+                        else {
+                            Icon(
+                                Icons.Filled.Info,
+                                contentDescription = "Localized description",
+                            )
+                        }
+                    },
+                    leadingContent = {
+                        Image(
+                            painter = painterResource(kniha.obrazok),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .padding(end = 16.dp)
+                                .size(40.dp)
+                                .clip(RoundedCornerShape(16.dp))
+                        )
+                    }
+                )
             }
-            Text(text = vyroky.get(0).toString())
         }
     }
 }
 
-@Preview(showBackground = true)
+
+@Preview(name = "Light Mode")
+/*@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)*/
 @Composable
 fun GreetingPreview() {
     VAMZAplikaciaTheme {
--- app/src/main/java/com/example/vamzaplikacia/ui/theme/Type.kt
+++ app/src/main/java/com/example/vamzaplikacia/ui/theme/Type.kt
@@ -5,30 +5,122 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.sp
+import com.example.vamzaplikacia.R
+
+val LibreBaskerville = FontFamily(
+    Font(R.font.libre_baskerville),
+    Font(R.font.libre_baskerville_italic, style = FontStyle.Italic),
+    Font(R.font.libre_baskerville_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+    displayLarge = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 48.sp,
+        lineHeight = 56.sp,
+        letterSpacing = 0.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    titleLarge = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = LibreBaskerville,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
     )
-    */
 )
