82edacb2371c6247238ebaee685b324e0451c271
huzevkova <huzevkova@stud.uniza.sk>
2024-04-07 18:26:45 +0200

    dokoncenie formulara - pridanie vyberu zo žánrov a vlastností

--- app/src/main/java/com/example/vamzaplikacia/MainActivity.kt
+++ app/src/main/java/com/example/vamzaplikacia/MainActivity.kt
@@ -4,37 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.vamzaplikacia.grafika.formular.FormularKniha
-import com.example.vamzaplikacia.logika.knihy.Kniha
-import com.example.vamzaplikacia.logika.knihy.Vstup
 import com.example.vamzaplikacia.logika.knihy.ZoznamKnih
 import com.example.vamzaplikacia.ui.theme.VAMZAplikaciaTheme
-import com.example.vamzaplikacia.grafika.zoznamiUI.VypisanieKnih
-import com.example.vamzaplikacia.grafika.zoznamiUI.VytvorZoznam
-import java.util.Date
 
 val zoznamKnih = ZoznamKnih()
 
