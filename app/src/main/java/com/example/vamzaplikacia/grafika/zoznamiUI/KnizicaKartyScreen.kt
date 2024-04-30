4c0317513d33b47ed2fb5093666bc6da2565534d
huzevkova <huzevkova@stud.uniza.sk>
2024-04-30 21:13:10 +0200

    Pridanie dialógového okna pre nový zoznam knih

--- app/src/main/java/com/example/vamzaplikacia/grafika/zoznamiUI/KnizicaKartyScreen.kt
+++ app/src/main/java/com/example/vamzaplikacia/grafika/zoznamiUI/KnizicaKartyScreen.kt
@@ -14,18 +14,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -34,16 +27,13 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.vamzaplikacia.grafika.formular.NovyZoznamDialog
-import com.example.vamzaplikacia.grafika.formular.NovyZoznamViewModel
 import com.example.vamzaplikacia.logika.knihy.Kniznica
 import com.example.vamzaplikacia.logika.knihy.ZoznamKnih
 
 val kniznica = Kniznica()
 
 @Composable
-fun KnizicaKartyScreen (showNovyZoznamDialog: Boolean = false, onClick: (ZoznamKnih) -> Unit) {
+fun KnizicaKartyScreen (onClick: (ZoznamKnih) -> Unit) {
     var pocetCol = 2
     var ratio = 0.8f
 
